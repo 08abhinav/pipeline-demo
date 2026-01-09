a271eb57a0d055810ea5665d910005f645b798d4
Ubuntu <ubuntu@ip-172-31-26-76.ec2.internal>
2026-01-09 09:23:58 +0000

    Reapply "custom package"
    This reverts commit 949c3b202cea85abfb5e20f17448b26bada712cb.

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt install tomcat -y
+sudo apt -y install httpd
+sudo cat << EOF > /etc/httpd/conf.d/tomcat_manager.conf
+<VirtualHost *:80>
+  ServerAdmin root@localhost
+  ServerName app.nextwork.com
+  DefaultType text/html
+  ProxyRequests off
+  ProxyPreserveHost On
+  ProxyPass / http://localhost:8080/nextwork-web-project/
+  ProxyPassReverse / http://localhost:8080/nextwork-web-project/
+</VirtualHost>
+EOF
